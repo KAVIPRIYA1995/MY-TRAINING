0691fad5d53c553cd696a081506c1b19e71bf746
KAVIPRIYA1995 <53401835+KAVIPRIYA1995@users.noreply.github.com>
2019-07-29 13:49:39 +0530

    10th Program

---
+++ NumberOfDigits.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main()
+{
+    int num, count=0;
+    cin>>num;
+    while(num>0)
+    {
+    	num=num/10;
+    	count++;
+    }
+    cout<<count;
+	return 0;
+}
