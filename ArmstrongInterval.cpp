661666f6e1483ee94d15910ce35dcbf584fd4e4b
KAVIPRIYA1995 <53401835+KAVIPRIYA1995@users.noreply.github.com>
2019-07-30 16:09:29 +0530

    18th Program

---
+++ ArmstrongInterval.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int a,b,i,n,d,s;
+	cin>>a>>b;
+	for(i=a;i<=b;i++)
+	{
+	s=0;n=i;
+	for(;n>0;n/=10)
+	{
+		d=n%10;
+		s=s+d*d*d;
+	}
+	if(s==i)
+	{
+		cout<<i;
+	}
+	}
+	return 0;
+}
